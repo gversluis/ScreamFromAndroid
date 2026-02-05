835784bda09d376fa0fba6939f83f43763321a57
gversluis <github@versluis.nl.eu.org>
2026-02-05 18:45:03 +0100

    Automatically choose build filename

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -14,7 +17,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -37,6 +40,18 @@ android {
     }
     buildFeatures {
         compose = false
+    }
+    applicationVariants.all {
+        outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            val date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+            val filename = when (buildType.name) {
+                "release" -> "screamfromandroid-v${versionName}.apk"
+                "debug" -> "screamfromandroid-v${versionName}-${buildType.name}.apk"
+                else -> "screamfromandroid-v${versionName}-${buildType.name}-${date}.apk"
+            }
+            output.outputFileName = filename
+        }
     }
 }
 
